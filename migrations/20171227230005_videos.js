3865a9f4f0e1f1b0d418fe64667754a8169fbc47
mawarren85 <mawarren85@gmail.com>
2018-01-05 12:09:06 -0600

    Final commit before presentation...wahh likes dont work

--- migrations/20171227230005_videos.js
+++ migrations/20171227230005_videos.js
@@ -1,12 +1,14 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('videos', (table) => {
-    table.increments('video_id');
+
     table.integer('user_id').unsigned();
     table.foreign('user_id').references('users.id').onDelete('cascade');
     table.integer('spot_id').unsigned();
     table.foreign('spot_id').references('spots.id').onDelete('cascade');
     table.integer('upvotes');
+    table.string('video');
+    table.timestamps(true, true);
   })
 };
 
--- public/js/main.js
+++ public/js/main.js
@@ -160,7 +160,7 @@ function addProfPhoto(albumName) {
  var file = files[0];
  var fileName = file.name;
  var albumPhotosKey = encodeURIComponent(albumName) + '//';
-alert(fileName)
+
  var photoKey = albumPhotosKey + fileName;
  s3.upload({
    Key: photoKey,
@@ -172,9 +172,11 @@ alert(fileName)
    }
 
      console.log(data.Location);
+     var html =      '<img src="' + data.Location + '">'
 
      $.post('/users/profile', {url: data.Location}, function(){
-         alert('done sending shit')
+            $('#profilePhoto').html(html)
+         alert('Photo successfully uploaded!')
         viewAlbum(albumName);
 
      })
@@ -204,22 +206,80 @@ function addPhoto(albumName) {
     let spot = $('#hidden-spot-id').text()
     let user = $('#hidden-user-id').text()
     var photo = data.location
+    let userName = $('#hidden-user-name').text()
 
      $.post('/users/location', {photo: data.Location, spot_id: spot, user_id: user})
      .done(function(results) {
        console.log(data)
        var html =         '<div class="tile is-ancestor">' +
-                 '<div class="tile is-parent is-vertical">' +
-                   '<div class="tile box is-child bgwhite">' +
-                     '<figure class="image is-1by2">' +
+         '<div class="tile is-parent">' +
+           '<div class="tile box is-child bgwhite">' +
 
-                         '<img src="' + data.Location + '">' +
+             '<figure class="image is-4by3">' +
 
 
-                     '</figure>' +
-                   '</div>' +
-                 '</div>' +
-               '</div>'
+                   '<img src="' + data.Location + '">' +
+
+             '</figure>' +
+
+             '<div class="tile is-child">' +
+               '<div class="columns">' +
+                 '<div class="column is-8">' +
+
+                   '<p class="top-space">' +
+                   '<h3 class="title is-5">' +
+
+               userName +  'added a photo' +
+             '</h3>' + '</p>'  +
+                 '<div class="subtitle is-6 grey">' +
+
+                 '</div>'  +
+
+              ' </div>'  +
+               '<div class="column">' +
+
+               '<div class="tile is-child is-right">' +
+
+                 '<div class="tile is-ancestor">'  +
+                   '<div class="tile is-parent">'  +
+                     '<div class="tile is-child  bgwhite">' +
+                      ' <p class="top-space">'  +
+
+                      ' <form class="" action="/users/location" method="post">' +
+                         '<div class="field">'  +
+
+                           '<input type="hidden" name="like" value="1">'  +
+                           '<button class="button is-primary is-outlined is-focus" type="submit">' + '&nbsp;&nbsp; Like&nbsp;' + '</button>'  +
+                         '</div>' +
+                       '</form>'  +
+                    ' </p>'  +
+                     '</div>'  +
+
+                     '<div class="tile is-child  bgwhite">'  +
+                      ' <p class="top-space">'  +
+                       '<form class="" action="/users/location" method="post">'  +
+                         '<div class="field">'  +
+
+                          ' <input type="hidden" name="follow" value="1">'  +
+                           '<button class="button is-primary is-outlined is-focus" type="submit">' + 'Follow' + '</button>'  +
+                         '</div>' +
+                       '</form>'  +
+                     '</p>' +
+                     '</div>'  +
+
+                     '</div>'  +
+                   '</div>'  +
+                 '</div>'+
+               '</div>' +
+               '</div>' +
+             '</div>' +
+
+
+
+
+           '</div>' +
+         '</div>' +
+       '</div>'
        $('#fakeUploadPhoto').append(html)
         $('#getPhotoForm').toggleClass('hidden')
          alert('Photo uploaded successfully!')
@@ -232,6 +292,126 @@ function addPhoto(albumName) {
 
  //});
 }
+
+
+function addVideo(albumName) {
+ var files = document.getElementById('spotvideoupload').files;
+ if (!files.length) {
+   return alert('Please choose a file to upload first.');
+ }
+
+ var file = files[0];
+ var fileName = file.name;
+ var albumPhotosKey = encodeURIComponent(albumName) + '//';
+
+ var photoKey = albumPhotosKey + fileName;
+ s3.upload({
+   Key: photoKey,
+   Body: file,
+   ACL: 'public-read'
+ }, function(err, data) {
+   if (err) {
+     return alert('There was an error uploading your photo: ', fileName);
+   }
+    let spot = $('#hidden-spot-id').text()
+    let user = $('#hidden-user-id').text()
+    let userName = $('#hidden-user-name').text()
+    var video = data.location
+
+     $.post('/users/location', {video: data.Location, spot_id: spot, user_id: user})
+     .done(function(results) {
+       console.log(data)
+       var html =
+                     '<div class="tile is-ancestor">' +
+                       '<div class="tile is-parent">' +
+                         '<div class="tile box is-child bgwhite">' +
+
+
+
+                             '<video width="100%" controls>' +
+                     '<source src="' + data.Location + '" type="video/mp4">' +
+                     '</video>' +
+
+
+
+                           '<div class="tile is-child">' +
+                             '<div class="columns">' +
+                               '<div class="column is-8">' +
+                                 '<p class="top-space">' +
+                                 '<h3 class="title is-5">' +
+
+                             userName +  'added a video' +
+                           '</h3>' + '</p>' +
+                               '<p class="subtitle is-6 grey">' +
+                                 // <%= video.updated_at %>
+                              ' </p>' +
+
+                             '</div>' +
+                             '<div class="column">' +
+
+                             '<div class="tile is-child is-right">' +
+
+                              ' <div class="tile is-ancestor">' +
+                                 '<div class="tile is-parent">' +
+                                   '<div class="tile is-child  bgwhite">' +
+
+                                     '<p class="top-space">' +
+                                     '<form class="" action="/users/location" method="post">' +
+                                       '<div class="field">' +
+
+                                         '<input type="hidden" name="like" value="1">' +
+                                         '<button class="button is-primary is-outlined is-focus" type="submit">' + '&nbsp;&nbsp; Like&nbsp;' + '</button>' +
+                                       '</div>' +
+                                     '</form>' +
+                                   '</p>' +
+                                   '</div>' +
+
+                                   '<div class="tile is-child  bgwhite">' +
+                                     '<p class="top-space">' +
+                                     '<form class="" action="/users/location" method="post">' +
+                                       '<div class="field">' +
+
+                                         '<input type="hidden" name="follow" value="1">' +
+                                         '<button class="button is-primary is-outlined is-focus" type="submit">' + 'Follow' + '</button>' +
+                                       '</div>' +
+                                    ' </p>' +
+                                     '</form>' +
+
+                                   '</div>' +
+
+                                   '</div>' +
+                                 '</div>' +
+                               '</div>'
+                             '</div>' +
+                             '</div>' +
+                           '</div>' +
+
+
+
+
+                         '</div>' +
+                       '</div>' +
+                     '</div>' +
+             '</div>'
+
+       $('#fakeUploadVideo').append(html)
+        $('#getVideoForm').toggleClass('hidden')
+         alert('Video uploaded successfully!')
+        viewAlbum(albumName);
+
+     })
+
+
+   })
+
+ //});
+}
+
+
+
+
+
+
 
 function deletePhoto(albumName, photoKey) {
  s3.deleteObject({Key: photoKey}, function(err, data) {
